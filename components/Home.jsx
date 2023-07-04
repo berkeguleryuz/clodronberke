dc01c9d8d89c84433801393447f81b7f60ec08ca
Berke Guleryuz <berkeguleryuz@hotmail.com>
2023-07-04 22:44:52 +0300

    update

--- components/Home.jsx
+++ components/Home.jsx
@@ -6,12 +6,16 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 import TextGradientComponent from '@/components/AnimatedText';
 
 export default function HomePage() {
+  var ReactRotatingText = require('react-rotating-text');
+
+
 
   return (
+    
     <div id="home" className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-            <h1 className='py-4 text-gray-700'>Hi, We are <span className="text-[#5651e5]">Clodron</span></h1>
+            <h1 className='py-4 text-gray-700'>Hi, We are <span className="text-[#5651e5]"><ReactRotatingText items={['Clodron', 'Your Marketing Partner', 'Your Web Agency']} /></span></h1>
             <TextGradientComponent />
             <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-dark'>
